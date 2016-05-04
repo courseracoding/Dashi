f86c3732e80777763e72977d043aef7465e9ff2b
steveny <s@g.c>
2016-05-04 09:03:04 -0700

    init files

---
+++ Dashi/src/api/RecommendRestaurants.java
@@ -0,0 +1,31 @@
+package api;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import db.DBConnection;
+import db.MySQLDBConnection;
+
+@WebServlet("/recommendation")
+public class RecommendRestaurants extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		DBConnection connection = new MySQLDBConnection();
+		JSONArray array = new JSONArray();
+		if (request.getParameterMap().containsKey("user_id")) {
+			String userId = request.getParameter("user_id");
+			array = connection.recommendRestaurants(userId);
+
+		}
+		RpcParser.writeOutput(response, array);
+	}
+}
